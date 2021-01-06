87ae98f61e285470361f0ba97d3caa0f9de84789
brahadeeshdrvs <brahadeeshdrvs@gmail.com>
2021-01-06 19:06:46 +0530

    initial commit

--- src/App.js
+++ src/App.js
@@ -1,25 +1,39 @@
 import logo from './logo.svg';
 import './App.css';
+import Cards from './components/Cards'
+import React, { Component } from 'react';
+import styles from './Card.module.css'
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+class App extends Component {
+  constructor(props) {
+    super(props);
+    this.state = { 
+      api : [],
+      loader: true
+    }
+  }
+  componentDidMount(){
+    fetch("https://www.omdbapi.com/?apikey=45f0782a&s=war")
+    .then(response => response.json())
+    .then(json => this.setState({api : json, loader:false}))
+  }
+  render() { 
+    
+    let cardArr = this.state.api.Search
+    console.log(this.state.api.Search)
+    return ( 
+      <div className={styles.Main}>
+          <div className={styles.CardGrid}>
+            {cardArr ?  cardArr.map(item => <Cards props={item}/>)   : null}
+          </div>
+       
+      </div>
+ 
+    );
 }
+}
+ 
 
 export default App;
+
+
